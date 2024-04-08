ead0b8f5511c5e7164c5725fecb11711c961d1c7
Diego Jimenez <151763153+diegojiimenez@users.noreply.github.com>
2024-04-08 12:39:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,41 +3,31 @@ const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
 
-// Crear una instancia de la aplicación Express
 const app = express();
 
-// Middleware para parsear solicitudes JSON
 app.use(bodyParser.json());
 
-// Conexión a la base de datos MongoDB
 const { connectToDatabase } = require('./modules/mongodb/mongodb.module');
 connectToDatabase();
 
-// Definir las rutas de la API
 const tareasRouter = require('./routes/tareas');
 app.use('/tareas', tareasRouter);
 
-// Configuración de la carpeta de archivos estáticos (si es necesario)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Configurar el motor de plantillas EJS
 app.set('view engine', 'ejs');
-// Configurar el directorio de vistas
 app.set('views', path.join(__dirname, 'views', 'pages'));
 
-// Ruta para renderizar el archivo index.ejs
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-// Manejador de errores para rutas no encontradas
 app.use((req, res, next) => {
   const error = new Error('Ruta no encontrada');
   error.status = 404;
   next(error);
 });
 
-// Middleware para manejar errores
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
   res.json({
