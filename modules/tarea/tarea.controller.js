43528c99096277ec868e240638b5d68be5338365
Antonio Gavilanes <antonio.gavilanes@alumnos.uneatlantico.es>
2024-04-16 23:38:39 +0200

    Fecha de entrega en las tareas

--- modules/tarea/tarea.controller.js
+++ modules/tarea/tarea.controller.js
@@ -4,27 +4,24 @@ const Responsable = require('../../models/responsable.model');
 exports.actualizarTarea = async (req, res) => {
   try {
     const { id } = req.params;
-    const { nombre, descripcion, responsableNombre, responsableApellido } = req.body;
+    const { nombre, descripcion, responsableNombre, responsableApellido, fechaEntrega } = req.body;
 
-    // Verificar si los campos del responsable están presentes en el cuerpo de la solicitud
-    if (!nombre || !descripcion || !responsableNombre || !responsableApellido) {
+    if (!nombre || !descripcion || !responsableNombre || !responsableApellido || !fechaEntrega) {
       return res.status(400).json({ error: 'Todos los campos son obligatorios' });
     }
 
-    // Buscar el responsable por nombre y apellido
     let responsable = await Responsable.findOne({ nombre: responsableNombre, apellido: responsableApellido });
 
-    // Si el responsable no existe, crearlo
     if (!responsable) {
       responsable = new Responsable({ nombre: responsableNombre, apellido: responsableApellido });
       await responsable.save();
     }
 
-    // Actualizar la tarea con el ID del responsable
     const tareaActualizada = await Tarea.findByIdAndUpdate(id, {
       nombre,
       descripcion,
-      responsable: responsable._id // Actualizar con el ID del responsable
+      responsable: responsable._id,
+      fechaEntrega
     }, { new: true });
 
     if (!tareaActualizada) {
@@ -37,7 +34,6 @@ exports.actualizarTarea = async (req, res) => {
     res.status(500).json({ error: 'Error al actualizar la tarea' });
   }
 };
-
 
 exports.eliminarTarea = async (req, res) => {
   try {
@@ -83,7 +79,8 @@ exports.obtenerTodasLasTareas = async (req, res) => {
       nombre: tarea.nombre,
       descripcion: tarea.descripcion,
       responsableNombre: tarea.responsable ? tarea.responsable.nombre : 'Responsable no especificado',
-      responsableApellido: tarea.responsable ? tarea.responsable.apellido : 'Responsable no especificado'
+      responsableApellido: tarea.responsable ? tarea.responsable.apellido : 'Responsable no especificado',
+      fechaEntrega: tarea.fechaEntrega
     }));
 
     res.json(tareasFormateadas);
@@ -97,9 +94,9 @@ exports.obtenerTodasLasTareas = async (req, res) => {
 
 exports.crearTarea = async (req, res) => {
   try {
-    const { nombre, descripcion, responsableNombre, responsableApellido } = req.body;
+    const { nombre, descripcion, responsableNombre, responsableApellido, fechaEntrega } = req.body;
 
-    if (!nombre || !descripcion || !responsableNombre || !responsableApellido) {
+    if (!nombre || !descripcion || !responsableNombre || !responsableApellido || !fechaEntrega) {
       return res.status(400).json({ error: 'Todos los campos son obligatorios' });
     }
 
@@ -110,7 +107,7 @@ exports.crearTarea = async (req, res) => {
       await responsable.save();
     }
 
-    const nuevaTarea = await Tarea.create({ nombre, descripcion, responsable: responsable._id });
+    const nuevaTarea = await Tarea.create({ nombre, descripcion, responsable: responsable._id, fechaEntrega });
 
     res.status(201).json({ mensaje: 'Tarea creada correctamente', tarea: nuevaTarea });
   } catch (error) {
